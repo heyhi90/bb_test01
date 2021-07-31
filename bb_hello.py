25f93df45b17f053b65fb2b480ad7950bae0b88f
ARA <ara90q@gmail.com>
2021-07-31 23:51:19 +0900

    TEST

--- bb_hello.py
+++ bb_hello.py
@@ -6,17 +6,20 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-chrome_options1 = Options()
-chrome_options1.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-chrome_options1.add_argument('--disable-gpu')
-chrome_options1.add_argument('--no-sandbox')
+options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36")
+options.add_argument("lang=ko");
 
 #텔레그램 환경변수
 github_T = os.environ.get('GIT_TOKEN')
 
 #driver 실행
-driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options1)
-driver.implicitly_wait(3)
+driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=options)
+driver.implicitly_wait(4)
 
 driver.get('https://bunker.blue/diary')
 driver.find_element_by_name('user_id').send_keys('asd12')
@@ -40,7 +43,9 @@ def ppompp():
         post_Num.append(postNum)
 
     print(post_Num[0])
-    testbot.sendMessage(1840767554, 'a')
+    testbot.sendMessage(1840767554, 'twewer')
+
+    driver.close()
 
 if __name__ == '__main__':
 
