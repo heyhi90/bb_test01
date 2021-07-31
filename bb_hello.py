6ebef63f57d8dd09b022d59343807ec620b8151a
ARA <ara90q@gmail.com>
2021-07-31 20:36:04 +0900

    TEST

--- bb_hello.py
+++ bb_hello.py
@@ -5,8 +5,8 @@ import os
 github_T = os.environ.get('GIT_TOKEN')
 
 def ppomppu():
-    testbot = telegram.Bot(token=github_T)
-    testbot.sendMessage(1840767554, 'a')
+    testbot = telegram.Bot(token='1902707442:AAHcK9oumcVMWUWJMAhk0JFj-aH-Gxp6e48')
+    testbot.sendMessage(1840767554, github_T)
 
 if __name__ == '__main__':
 
