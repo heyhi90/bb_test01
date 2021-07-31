0bc3fdbbda1bf511a0b7fb4bd09cc5d346da3e0a
ARA <ara90q@gmail.com>
2021-07-31 20:59:12 +0900

    TEST

--- bb_hello.py
+++ bb_hello.py
@@ -1,16 +1,15 @@
 #패키지 임폴트
 import telegram
-import os
 
 #github_T = os.environ.get('GIT_TOKEN')
 
-def ppomppu():
+def ppompp():
     testbot = telegram.Bot(token='1902707442:AAHcK9oumcVMWUWJMAhk0JFj-aH-Gxp6e48')
     testbot.sendMessage(1840767554, 'test1')
 
 if __name__ == '__main__':
 
     try:
-        ppomppu()
+        ppompp()
     except AttributeError as e:
         print(e)
